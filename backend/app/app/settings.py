29565e69fe3027b85ba7bb80cff18d76094c9e43
HiagoAC <68502300+HiagoAC@users.noreply.github.com>
2025-10-23 22:12:22 +0900

    Replace hardcoded SECRET_KEY with environment variable

--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#q(a4qto=9*+@+ja$$d5u=hm65mbtby7)4qbdw*+el)qxl(k$8'
+SECRET_KEY = os.environ.get('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
