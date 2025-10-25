f511c4fbddfbb5100ec876d8a038b0e9082120df
HiagoAC <68502300+HiagoAC@users.noreply.github.com>
2025-10-25 23:14:17 +0900

    Refactor fetchBoards function to use consistent variable naming

--- frontend/app/src/features/board/services.ts
+++ frontend/app/src/features/board/services.ts
@@ -3,6 +3,6 @@ import apiClient from "../../services/apiClient";
 const BOARD_URL = "boards/";
 
 export const fetchBoards = async () => {
-	const response = await apiClient.get(BOARD_URL);
-	return response.data;
+	const res = await apiClient.get(BOARD_URL);
+	return res.data;
 };
