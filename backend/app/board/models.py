f378e58d3ef58b3c3e15a9690cb63dcbee53b55c
HiagoAC <68502300+HiagoAC@users.noreply.github.com>
2025-10-25 22:07:56 +0900

    Fix linting

--- backend/app/board/models.py
+++ backend/app/board/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from ordered_model.models import OrderedModel # type: ignore
+from ordered_model.models import OrderedModel  # type: ignore
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
