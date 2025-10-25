39955445f05d9205240c403cf82ca680450e46d2
HiagoAC <68502300+HiagoAC@users.noreply.github.com>
2025-10-25 22:08:54 +0900

    Add exception handler for IntegrityError in API

--- backend/app/app/api.py
+++ backend/app/app/api.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError
 from ninja import NinjaAPI
 
 from board.api import board_router
@@ -6,3 +7,12 @@ from board.api import board_router
 api = NinjaAPI(urls_namespace='api')
 
 api.add_router('boards/', board_router)
+
+
+@api.exception_handler(IntegrityError)
+def on_integrity_error(request, exc):
+    return api.create_response(
+        request,
+        {"detail": "Invalid data. Please check your input."},
+        status=400,
+    )
