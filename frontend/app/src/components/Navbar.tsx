014665f58ac6a1557ec95d6b452180d783ef8ee3
HiagoAC <68502300+HiagoAC@users.noreply.github.com>
2025-10-27 22:55:23 +0900

    Update Navbar title to 'Kanban Boards'

--- frontend/app/src/components/Navbar.tsx
+++ frontend/app/src/components/Navbar.tsx
@@ -1,18 +1,17 @@
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-
+import AppBar from "@mui/material/AppBar";
+import Button from "@mui/material/Button";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 
 export function Navbar() {
-    return (
-        <AppBar position="static" sx={{ m: 0, p: 0 }}>
-        <Toolbar>
-            <Typography variant="h6" sx={{ flexGrow: 1 }}>
-                My App
-            </Typography>
-            <Button color="inherit">Login</Button>
-        </Toolbar>
-        </AppBar>
-    );
+	return (
+		<AppBar position="static" sx={{ m: 0, p: 0 }}>
+			<Toolbar>
+				<Typography variant="h6" sx={{ flexGrow: 1 }}>
+					Kanban Boards
+				</Typography>
+				<Button color="inherit">Login</Button>
+			</Toolbar>
+		</AppBar>
+	);
 }
