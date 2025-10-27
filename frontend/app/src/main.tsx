1f163b344f7299f3d4169ed06b3345552d264cf9
HiagoAC <68502300+HiagoAC@users.noreply.github.com>
2025-10-27 22:55:53 +0900

    Add main.css for global styles

--- frontend/app/src/main.tsx
+++ frontend/app/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import "./main.css";
 
 // biome-ignore lint/style/noNonNullAssertion: Vite template uses non-null assertion for root element
 createRoot(document.getElementById("root")!).render(
