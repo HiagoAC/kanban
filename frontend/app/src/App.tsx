45c35df2f40482e19e04671442cca72205c34093
HiagoAC <68502300+HiagoAC@users.noreply.github.com>
2025-10-29 00:40:35 +0900

    Wrap App component with QueryClientProvider for React Query integration

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,16 +1,21 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { BoardPage } from "./pages/BoardPage";
 import { HomePage } from "./pages/HomePage";
 import { NewBoardPage } from "./pages/NewBoardPage";
 
+const queryClient = new QueryClient();
+
 export default function App() {
 	return (
-		<Router>
-			<Routes>
-				<Route path="/" element={<HomePage />} />
-				<Route path="/board/:id" element={<BoardPage />} />
-				<Route path="/new-board" element={<NewBoardPage />} />
-			</Routes>
-		</Router>
+		<QueryClientProvider client={queryClient}>
+			<Router>
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/board/:id" element={<BoardPage />} />
+					<Route path="/new-board" element={<NewBoardPage />} />
+				</Routes>
+			</Router>
+		</QueryClientProvider>
 	);
 }
