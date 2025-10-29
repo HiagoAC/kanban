ec63653d313ecb1ee45e2264e75fdda019b707cd
HiagoAC <68502300+HiagoAC@users.noreply.github.com>
2025-10-29 23:22:54 +0900

    Add SideBarListButton component

---
+++ frontend/app/src/components/SideBarListButton.tsx
@@ -0,0 +1,31 @@
+import { ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+
+interface SideBarListButtonProps {
+    onClick: () => void;
+    text: string;
+    icon?: React.ReactNode;
+}
+
+export function SideBarListButton({ onClick, text, icon }: SideBarListButtonProps) {
+    return (
+        <ListItemButton onClick={onClick}>
+            {icon && (
+                <ListItemIcon sx={{ minWidth: "32px", marginRight: "4px" }}>
+                    {icon}
+                </ListItemIcon>
+            )}
+            <ListItemText
+                primary={text}
+                slotProps={{
+                        primary: {
+                            style: {
+                                fontSize: "14px",
+                                fontWeight: 500,
+                                color: "#333",
+                            },
+                        },
+                    }}
+                />
+            </ListItemButton>
+    );
+}
