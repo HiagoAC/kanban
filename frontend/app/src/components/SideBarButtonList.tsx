2d6932154c25a47f2fbd2f2ebc94300cf6bf3f52
HiagoAC <68502300+HiagoAC@users.noreply.github.com>
2025-11-18 18:41:55 +0900

    Add search and home buttons to side bar

--- frontend/app/src/components/SideBarButtonList.tsx
+++ frontend/app/src/components/SideBarButtonList.tsx
@@ -1,4 +1,4 @@
-import { Add as AddIcon } from "@mui/icons-material";
+import { Add as AddIcon, Search as SearchIcon, Home as HomeIcon } from "@mui/icons-material";
 import { List, ListItem } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { SideBarListButton } from "./SideBarListButton";
@@ -13,6 +13,20 @@ export function SideBarButtonList() {
 					onClick={() => navigate("/new-board")}
 					text="Create New Board"
 					icon={<AddIcon />}
+				/>
+			</ListItem>
+			<ListItem disablePadding>
+				<SideBarListButton
+					onClick = {() => {}}
+					text="Search"
+					icon={<SearchIcon />}
+				/>
+			</ListItem>
+			<ListItem disablePadding>
+				<SideBarListButton
+					onClick={() => navigate("/")}
+					text="Home"
+					icon={<HomeIcon />}
 				/>
 			</ListItem>
 		</List>
