6462fe38f3a5a9d6ab16478a636a5b95253acddf
HiagoAC <68502300+HiagoAC@users.noreply.github.com>
2025-11-29 23:52:39 +0900

    Add CardItem component for displaying individual cards

---
+++ frontend/app/src/features/card/components/CardItem.tsx
@@ -0,0 +1,35 @@
+import CircleIcon from "@mui/icons-material/Circle";
+import { Paper, Stack, Typography } from "@mui/material";
+import type { CardListItem } from "../types";
+
+interface CardItemProps {
+	card: CardListItem;
+}
+
+export function CardItem({ card }: CardItemProps) {
+	const priorityToChipColor = {
+		high: "error",
+		medium: "warning",
+		low: "info",
+	} as const;
+
+	const priorityColor = card.priority
+		? priorityToChipColor[card.priority]
+		: "primary";
+
+	return (
+		<Paper elevation={3} sx={{ p: 2, mt: 2, mx: 2, bgcolor: "lightgrey" }}>
+			<Stack>
+				<Typography variant="subtitle1" fontWeight="bold">
+					{card.title}
+				</Typography>
+				<Stack direction="row" spacing={0.2} my={1} alignItems="center">
+					<CircleIcon sx={{ fontSize: 12 }} color={priorityColor} />
+					<Typography sx={{ fontSize: 12, color: "text.secondary" }}>
+						{card.priority}-priority
+					</Typography>
+				</Stack>
+			</Stack>
+		</Paper>
+	);
+}
