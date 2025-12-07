2a266f43192e709d595ba3d822895c34b5c8d81a
HiagoAC <68502300+HiagoAC@users.noreply.github.com>
2025-12-07 23:25:18 +0900

    Refactor CardItem component to include navigation buttons for previous and next columns

--- frontend/app/src/features/card/components/CardItem.tsx
+++ frontend/app/src/features/card/components/CardItem.tsx
@@ -1,33 +1,75 @@
-import { ButtonBase, Paper, Stack, Typography } from "@mui/material";
+import { Box, ButtonBase, Grid, IconButton, Paper, Stack, Typography } from "@mui/material";
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
 import { useNavigate } from "react-router-dom";
 import type { CardListItem } from "../types";
 import { PriorityDot } from "./PriorityDot";
+import { useUpdateCard } from "../hooks/useUpdateCard";
 
 interface CardItemProps {
 	card: CardListItem;
+	columnId: string;
+	nextColumnId?: string;
+	prevColumnId?: string;
 }
 
-export function CardItem({ card }: CardItemProps) {
+export function CardItem({ card, columnId, nextColumnId, prevColumnId }: CardItemProps) {
 	const navigate = useNavigate();
+	const { mutate: updateCard } = useUpdateCard({ originalColumnId: columnId });
 
 	return (
 		<Paper
 			component={ButtonBase}
 			onClick={() => navigate(`/cards/${card.id}`)}
 			elevation={3}
-			sx={{ p: 2, mt: 2, mx: 2, bgcolor: "lightgrey" }}
+			sx={{
+				py: 2,
+				mt: 2,
+				mx: 2,
+				bgcolor: "lightgrey",
+			}}
 		>
-			<Stack>
-				<Typography variant="subtitle1" fontWeight="bold">
-					{card.title}
-				</Typography>
-				<Stack direction="row" spacing={0.2} my={1} alignItems="center">
-					<PriorityDot priority={card.priority} />
-					<Typography sx={{ fontSize: 12, color: "text.secondary" }}>
-						{card.priority}-priority
-					</Typography>
-				</Stack>
-			</Stack>
+			<Grid
+				container
+				alignItems="center"
+				justifyContent="space-between"
+				wrap="nowrap"
+				sx={{ width: "100%" }}
+			>
+				<Grid size={2} alignContent="center">
+					{prevColumnId && <IconButton
+						onClick={(e) => {
+							e.stopPropagation();
+							updateCard({ id: card.id, cardData: { columnId: prevColumnId } });
+						}}
+					>
+						<ArrowLeftIcon />
+					</IconButton>}
+				</Grid>
+				<Grid size={8}>
+					<Stack alignItems="center">
+						<Typography variant="subtitle1" fontWeight="bold">
+							{card.title}
+						</Typography>
+						<Stack direction="row" spacing={0.2} my={1} alignItems="center">
+							<PriorityDot priority={card.priority} />
+							<Typography sx={{ fontSize: 12, color: "text.secondary" }}>
+								{card.priority}-priority
+							</Typography>
+						</Stack>
+					</Stack>
+				</Grid>
+				<Grid size={2} alignContent="center">
+					{nextColumnId && <IconButton
+						onClick={(e) => {
+							e.stopPropagation();
+							updateCard({ id: card.id, cardData: { columnId: nextColumnId } });
+						}}
+					>
+						<ArrowRightIcon />
+					</IconButton>}
+				</Grid>
+			</Grid>
 		</Paper>
 	);
 }
