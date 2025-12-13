4b2be8deed0003dbe0f97e4536caf8bc1e64b55d
HiagoAC <68502300+HiagoAC@users.noreply.github.com>
2025-12-13 22:54:04 +0900

    Sort boards by 'starred' status in SideBarBoardList component

--- frontend/app/src/features/board/components/SideBarBoardList.tsx
+++ frontend/app/src/features/board/components/SideBarBoardList.tsx
@@ -1,4 +1,6 @@
-import { Divider, List, ListItem } from "@mui/material";
+import StarIcon from "@mui/icons-material/Star";
+import { List, ListItem } from "@mui/material";
+import { useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import { SideBarListButton } from "../../../components/SideBarListButton";
 import { useFetchBoards } from "../hooks/useFetchBoards";
@@ -6,6 +8,12 @@ import { useFetchBoards } from "../hooks/useFetchBoards";
 export function SideBarBoardList() {
 	const navigate = useNavigate();
 	const query = useFetchBoards();
+
+	const sortedBoards = useMemo(() => {
+		return query.data
+			?.slice()
+			.sort((a, b) => Number(b.starred) - Number(a.starred));
+	}, [query.data]);
 
 	if (query.isLoading) {
 		return <div data-testid="loading">Loading...</div>;
@@ -17,15 +25,15 @@ export function SideBarBoardList() {
 
 	return (
 		<List>
-			{query.data?.map((board) => (
+			{sortedBoards?.map((board) => (
 				<ListItem key={board.id} disablePadding>
 					<SideBarListButton
 						onClick={() => navigate(`/boards/${board.id}`)}
 						text={board.title}
+						icon={board.starred ? <StarIcon fontSize="small" /> : null}
 					/>
 				</ListItem>
 			))}
-			<Divider variant="inset" component="li" />
 		</List>
 	);
 }
