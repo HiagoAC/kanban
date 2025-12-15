8ba1b15253d7653cd740ad3ec981633324b54af0
HiagoAC <68502300+HiagoAC@users.noreply.github.com>
2025-12-15 22:27:22 +0900

    Display LatestBoard in HomePage

--- frontend/app/src/pages/HomePage.tsx
+++ frontend/app/src/pages/HomePage.tsx
@@ -1,11 +1,10 @@
 import { Layout } from "../components/Layout";
+import { LatestBoard } from "../features/board/components/LatestBoard";
 
 export function HomePage() {
 	return (
 		<Layout isHomePage={true}>
-			<div>
-				<h1>Home Page</h1>
-			</div>
+			<LatestBoard />
 		</Layout>
 	);
 }
