31dfe5245bc30eea065a34745c39aab225e54424
HiagoAC <68502300+HiagoAC@users.noreply.github.com>
2026-01-03 11:58:13 +0900

    Fix linting

--- backend/app/user/pipeline.py
+++ backend/app/user/pipeline.py
@@ -4,21 +4,19 @@ from .services import create_user_with_board
 User = get_user_model()
 
 
-def create_user_with_board_pipeline(strategy, details, backend, user=None, *args, **kwargs):
+def create_user_with_board_pipeline(
+        strategy, details, backend, user=None, *args, **kwargs):
     """Custom social auth pipeline step to create user with default board."""
     if user is None:
         username = details.get('username') or details.get('email')
         if not username:
             return
-        
         if User.objects.filter(username=username).exists():
             return
-        
         user = create_user_with_board(username=username, password=None)
-        
         return {
             'is_new': True,
             'user': user
         }
-    
+
     return {'is_new': False, 'user': user}
