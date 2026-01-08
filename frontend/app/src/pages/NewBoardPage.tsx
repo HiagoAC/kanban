68ff2d5048e91f0590df1b4652a249c84533a955
HiagoAC <68502300+HiagoAC@users.noreply.github.com>
2026-01-09 01:53:11 +0900

    Refactor NewBoardPage layout to include button to home page in mobile view

--- frontend/app/src/pages/NewBoardPage.tsx
+++ frontend/app/src/pages/NewBoardPage.tsx
@@ -1,41 +1,57 @@
-import { Box, Paper, Typography } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Box, IconButton, Paper, Stack, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 import { Layout } from "../components/Layout";
 import { CreateBoardForm } from "../features/board/components/CreateBoardForm";
 
 export function NewBoardPage() {
+	const navigate = useNavigate();
+
 	return (
 		<Layout>
-			<Box
-				sx={{
-					display: "flex",
-					flexDirection: "column",
-					alignItems: "center",
-					justifyContent: "flex-start",
-					minHeight: "100vh",
-					gap: 4,
-					textAlign: "center",
-					px: 4,
-					pt: 6,
-				}}
-			>
-				<Typography variant="h3" fontWeight="bold">
-					Create New Kanban Board
-				</Typography>
-				<Paper
-					elevation={3}
+			<Stack direction="column">
+				<IconButton
+					onClick={() => navigate("/")}
 					sx={{
-						p: 4,
-						width: "100%",
-						maxWidth: 600,
-						borderRadius: 2,
-						display: "flex",
-						alignItems: "center",
-						justifyContent: "center",
+						ml: 2,
+						mr: "auto",
+						display: { xs: "flex", md: "none" },
 					}}
 				>
-					<CreateBoardForm />
-				</Paper>
-			</Box>
+					<MenuIcon />
+				</IconButton>
+				<Box
+					sx={{
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+						justifyContent: "flex-start",
+						minHeight: "100vh",
+						gap: 4,
+						textAlign: "center",
+						px: 4,
+						pt: 6,
+					}}
+				>
+					<Typography variant="h3" fontWeight="bold">
+						Create New Kanban Board
+					</Typography>
+					<Paper
+						elevation={3}
+						sx={{
+							p: 4,
+							width: "100%",
+							maxWidth: 600,
+							borderRadius: 2,
+							display: "flex",
+							alignItems: "center",
+							justifyContent: "center",
+						}}
+					>
+						<CreateBoardForm />
+					</Paper>
+				</Box>
+			</Stack>
 		</Layout>
 	);
 }
