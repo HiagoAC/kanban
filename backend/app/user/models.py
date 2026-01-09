4c73844b8803b9012fbd72f19eb44d4dd18812b6
HiagoAC <68502300+HiagoAC@users.noreply.github.com>
2026-01-09 19:46:19 +0900

    Add is_guest field to User model

--- backend/app/user/models.py
+++ backend/app/user/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     """Custom user model with additional fields."""
     avatar_url = models.URLField(blank=True, null=True, max_length=500)
+    is_guest = models.BooleanField(default=False)
 
     def __str__(self):
         return self.username
