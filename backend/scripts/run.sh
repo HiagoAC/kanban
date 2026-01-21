0d3f34b1bbd373109a43f94114619167bf0dfceb
HiagoAC <68502300+HiagoAC@users.noreply.github.com>
2026-01-22 05:57:30 +0900

    Fix uwsgi binding address in run.sh

--- backend/scripts/run.sh
+++ backend/scripts/run.sh
@@ -5,4 +5,4 @@ set -e
 python manage.py wait_for_db
 python manage.py migrate
 
-uwsgi --http :$PORT --module app.wsgi --workers 1 --enable-threads --master
+uwsgi --http 0.0.0.0:$PORT --module app.wsgi --workers 1 --enable-threads --master
