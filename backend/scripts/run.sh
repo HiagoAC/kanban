9945a63d126ed29880c4ba0782215b79f4802ba5
HiagoAC <68502300+HiagoAC@users.noreply.github.com>
2026-01-22 06:30:24 +0900

    Replace uWSGI with Gunicorn for application server

--- backend/scripts/run.sh
+++ backend/scripts/run.sh
@@ -5,4 +5,4 @@ set -e
 python manage.py wait_for_db
 python manage.py migrate
 
-uwsgi --http 0.0.0.0:$PORT --module app.wsgi --workers 1 --enable-threads --master
+gunicorn app.wsgi --bind 0.0.0.0:$PORT
