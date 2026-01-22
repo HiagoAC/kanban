a40e7ff0a800207f4b4b6a7fdace0229487d87c8
HiagoAC <68502300+HiagoAC@users.noreply.github.com>
2026-01-23 02:19:06 +0900

    Add STATIC_ROOT setting and configure static file serving in DEBUG mode

--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -183,3 +183,4 @@ if not DEBUG:
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
--- backend/app/app/urls.py
+++ backend/app/app/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from .api import api
 
 urlpatterns = [
@@ -23,3 +25,9 @@ urlpatterns = [
     path('api/', api.urls),
     path('social-auth/', include('social_django.urls', namespace='social')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
