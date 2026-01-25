f65c3abc27b496f1b865700ebb4de6de2fb0c8e0
HiagoAC <68502300+HiagoAC@users.noreply.github.com>
2026-01-25 22:36:45 +0900

    Update run.sh to include --noinput flag for collectstatic and specify gunicorn binding port

--- backend/scripts/run.sh
+++ backend/scripts/run.sh
@@ -3,7 +3,7 @@
 set -e
 
 python manage.py wait_for_db
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 python manage.py migrate
 
-gunicorn app.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn app.wsgi:application --bind 0.0.0.0:8000
