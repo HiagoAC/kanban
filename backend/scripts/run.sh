17f6dfbb77755b442f32a7e6736140ee42e44148
HiagoAC <68502300+HiagoAC@users.noreply.github.com>
2026-01-28 05:18:08 +0900

    Add deployment check to run script

--- backend/scripts/run.sh
+++ backend/scripts/run.sh
@@ -9,5 +9,6 @@ fi
 python manage.py wait_for_db
 python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py check --deploy
 
 gunicorn app.wsgi:application --bind 0.0.0.0:8000
