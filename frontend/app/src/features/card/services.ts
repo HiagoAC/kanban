81786d9662c491b7b60e149b0fb5ee3ed3c739c0
HiagoAC <68502300+HiagoAC@users.noreply.github.com>
2026-02-03 10:20:01 +0900

    Remove debug log from createCard function

--- frontend/app/src/features/card/services.ts
+++ frontend/app/src/features/card/services.ts
@@ -18,7 +18,6 @@ export const fetchCards = async (
 export const createCard = async (
 	card_data: CreateCardSchema,
 ): Promise<Card> => {
-	console.log("Creating card with data:", card_data);
 	const res = await apiClient.post<Card>(CARD_URL, card_data);
 	const card = res.data;
 	card.createdAt = new Date(card.createdAt);
